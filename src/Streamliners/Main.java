5957ffd45e3223eca41eff0fa5deb77697e2a175
Hrithik Rohilla <59164922+Crypt0Nyt@users.noreply.github.com>
2021-05-04 20:34:58 +0530

    Working on Version @

--- src/Streamliners/Main.java
+++ src/Streamliners/Main.java
@@ -30,62 +30,6 @@ public class Main {
         System.out.println(products);
 
 
-        /*
-         Null pointer exception:
-         Product product1 = new Product();
-         System.out.println(product1.name.toUpperCase(Locale.ROOT));
-         */
-
-
-
-        /*
-        Array listed Products:
-        List<Product> products = new ArrayList<>(
-                Arrays.asList(
-                        new Product("A","")
-                        ,new Product("B","")
-                        ,new Product("C","")
-                ));
-        System.out.println(products);
-        -----------------------------------------------------------------------------------------------------------
-        OUTPUT = [Product{name='A', imageURL=''}, Product{name='B', imageURL=''}, Product{name='C', imageURL=''}]
-        -----------------------------------------------------------------------------------------------------------
-        */
-
-
-
-        /*
-        List<Variant> variants = new ArrayList<>(
-                Arrays.asList(new Variant("500g",90),new Variant("2kg",180))
-        );
-
-        VariantsBasedProduct product = new VariantsBasedProduct(
-                "Kiwi","",variants);
-
-        System.out.println(product);
-        ----------------------------------------------------------------------------------------------------------------------------------------
-        OUTPUT = VariantsBasedProduct{name='Kiwi', imageURL='', variants=[Variant{name='500g', price=90.0}, Variant{name='2kg', price=180.0}]}
-        ----------------------------------------------------------------------------------------------------------------------------------------
-        */
-
-
-
-        /*
-        Equate Hashcode
-        Product a = new Product("a","");
-        Product b = new Product("a","");
-
-        System.out.println(a.equals(b));
-        --------------------------------
-        OUTPUT = true
-        --------------------------------
-        */
-
-
-
-
-
-
 
     }
 
