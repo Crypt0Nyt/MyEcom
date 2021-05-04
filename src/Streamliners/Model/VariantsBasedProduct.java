574f1b07fa501bfce5eb8d7bd82b9b52e9cdb1da
Hrithik Rohilla <59164922+Crypt0Nyt@users.noreply.github.com>
2021-05-04 20:28:19 +0530

    VariantsBasedProduct

---
+++ src/Streamliners/Model/VariantsBasedProduct.java
@@ -0,0 +1,24 @@
+package Streamliners.Model;
+
+import java.util.List;
+
+public class VariantsBasedProduct extends Product{
+    List<Variant> variants;
+
+    public VariantsBasedProduct(String name, String imageURL, List<Variant> variants) {
+        super(name,imageURL);
+        this.variants = variants;
+    }
+
+    @Override
+    public String toString() {
+        return "\nVariantsBasedProduct{" +
+                "name='" + name + '\'' +
+                ", imageURL='" + imageURL + '\'' +
+                ",\nvariants= " + variants +
+                '}'+"\n";
+    }
+}
+
+
+
